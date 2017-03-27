b003741c4b3c6d4fe6fd0b1512778c992c70e52d
bseeker <rodrigoc@galileo.edu>
2017-03-26 22:06:13 -0600

    think it's done

--- app/src/main/java/edu/galileo/rodrigo/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/galileo/rodrigo/tipcalculator/MainActivity.java
@@ -1,13 +1,118 @@
 package edu.galileo.rodrigo.tipcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etBillAmount)
+    EditText etBillAmount;
+    @BindView(R.id.tvTipPercent)
+    TextView tvTipPercent;
+    @BindView(R.id.tvTipAmount)
+    TextView tvTipAmount;
+    @BindView(R.id.tvBillTotalAmount)
+    TextView tvBillTotalAmount;
+
+    float percentage = 0;
+    float tipTotal = 0;
+    float finalBillAmount = 0;
+    float totalBillAmount = 0;
+
+    final float REGULAR_TIP_PERCENTAGE = 10;
+    final float DEFAULT_TIP_PERCENTAGE = 15;
+    final float EXCELLENT_TIP_PERCENTAGE = 20;
+    @BindView(R.id.tvBillAmount)
+    TextView tvBillAmount;
+    @BindView(R.id.vRatingButtons)
+    View vRatingButtons;
+    @BindView(R.id.ibRegularService)
+    ImageButton ibRegularService;
+    @BindView(R.id.ibGoodService)
+    ImageButton ibGoodService;
+    @BindView(R.id.ibExcellentService)
+    ImageButton ibExcellentService;
+    @BindView(R.id.rlRatingButtons)
+    LinearLayout rlRatingButtons;
+    @BindView(R.id.vRatingSeparator)
+    View vRatingSeparator;
+    @BindView(R.id.tvServiceRating)
+    TextView tvServiceRating;
+    @BindView(R.id.llTipInformation)
+    LinearLayout llTipInformation;
+    @BindView(R.id.vBillSeparator)
+    View vBillSeparator;
+    @BindView(R.id.tvBillTotal)
+    TextView tvBillTotal;
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    private void setTipValues() {
+        DecimalFormat df = new DecimalFormat("#0.00");
+        tvTipPercent.setText(getString(R.string.main_msg_tippercent, percentage));
+        tvTipAmount.setText(getString(R.string.main_msg_tiptotal, df.format(tipTotal)));
+        tvBillTotalAmount.setText(getString(R.string.main_msg_billtotalresult, df.format(finalBillAmount)));
+    }
+
+    @OnClick({R.id.ibRegularService, R.id.ibGoodService, R.id.ibExcellentService})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ibRegularService:
+                percentage = REGULAR_TIP_PERCENTAGE;
+                break;
+            case R.id.ibGoodService:
+                percentage = DEFAULT_TIP_PERCENTAGE;
+                break;
+            case R.id.ibExcellentService:
+                percentage = EXCELLENT_TIP_PERCENTAGE;
+                break;
+        }
+
+        calculateFinalBill();
+        setTipValues();
+    }
+
+
+    private void calculateFinalBill() {
+
+        if (percentage == 0)
+            percentage = DEFAULT_TIP_PERCENTAGE;
+
+        if (etBillAmount.getText().toString().equals("") || etBillAmount.getText().toString().equals("."))
+            totalBillAmount = 0;
+        else
+            totalBillAmount = Float.valueOf(etBillAmount.getText().toString());
+
+        tipTotal = (totalBillAmount * percentage) / 100;
+        finalBillAmount = totalBillAmount + tipTotal;
+
+    }
+
+    @OnTextChanged(R.id.etBillAmount)
+    public void OnTextChanged() {
+        calculateFinalBill();
+        setTipValues();
+    }
+
 }
